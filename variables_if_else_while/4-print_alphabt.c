1c95d4b69b5bc414f985de49c285f1cf98f80b1f
Yoruan <yoruanorta50@gmail.com>
2024-10-22 10:25:09 -0400

    T4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str[] = "abcdfghijklmnopqrstuvwxyz";
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+putchar(str[i]);
+putchar('\n');
+
+return (0);
+}
