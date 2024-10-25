4f5647bc32b9f584bef401ace6b38dc50e9cbaf5
Yoruan <yoruanorta50@gmail.com>
2024-10-25 12:03:31 -0400

    Making the task 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *times_table - prints the 9 times table, starting with 0
+ *
+**/
+
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			product = i * j;
+			if (j == 0)
+			printf("%d", product);
+			else
+			printf(", %2d", product);
+		}
+	printf("\n");
+	}
+}
