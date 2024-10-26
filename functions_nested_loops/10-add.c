c1a2cd5cd76e95d79128317d9d25b60daeaac104
Yoruan <yoruanorta50@gmail.com>
2024-10-26 11:22:26 -0400

    Making the task 10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * int_add - adds two integers
+ * add - adds two integers
  * @a: The first integer
  * @b: The second integer
  *
  * Return: The sum of a and b
-**/
-int int_add(int a, int b)
+ */
+int add(int a, int b)
 {
 	return (a + b);
 }
