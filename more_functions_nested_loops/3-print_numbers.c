2fc04ab9e36287f79ce71608ada49843f325b4a3
Yoruan <yoruanorta50@gmail.com>
2024-10-28 13:56:18 -0400

    Making the task 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbres form 0 to 9 followed by a new line
+ * print_numbers - prints the numbres form 0 to 9 followed by a dollar sign
  *
  * Return: void
 **/
 
 void print_numbers(void)
 {
-	char digits[] = "0123456789";
+	char output[] = "0123456789$";
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 11; i++)
 	{
-		_putchar(digits[i]);
+		_putchar(output[i]);
 	}
-	_putchar('$');
 	_putchar('\n');
 }
