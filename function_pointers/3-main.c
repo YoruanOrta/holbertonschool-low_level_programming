bd286adf00a3ae90760dd673d8bcf8ea6f0c74f0
Yoruan <yoruanorta50@gmail.com>
2024-11-20 13:34:29 -0400

    Making the most harder task ever(3)

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,12 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) 
+/**
+ * main - entry point
+ * @argc: arguments count
+ * @argv: arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
 {
 	int num1, num2;
 	int (*operation)(int, int);
 
-	if (argc != 4) 
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
@@ -18,13 +26,13 @@ int main(int argc, char *argv[])
 
 	operation = get_op_func(argv[2]);
 
-	if (operation == NULL) 
+	if (operation == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0) 
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
@@ -32,5 +40,5 @@ int main(int argc, char *argv[])
 
 	printf("%d\n", operation(num1, num2));
 
-	return 0;
+	return (0);
 }
