9ff25d4bda2f85fa54d3499b1f42b75042db8f66
Yoruan <yoruanorta50@gmail.com>
2024-12-03 11:58:11 -0400

    Making the task 0

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -15,10 +17,6 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
